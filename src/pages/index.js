a32230e66e11df053895fe341a73b9f9e7f66b00
tresor <tshims79@gmail.com>
2023-05-17 14:41:22 +0200

    I changed the navigation menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
 // Step 1: Import React
 import * as React from "react";
-import { Link } from "gatsby";
+import Layout from "../components/layout";
 
 // Step 2: Define your componet
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Welcome to Gatsby site!</h1>
-      <Link to="/about">About</Link>
+    <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
+    </Layout>
   );
 };
 // You'll learn about this in the next task, just copy it for me
